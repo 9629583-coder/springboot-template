7722994f8fd8a45e676a6119a799c7920da154fa
Schizox3BEBE <9629583@gmail.com>
2026-03-06 17:05:07 +0800

    init: 初始化 Spring Boot Web 学习项目

---
+++ week01/hello-project/Springboot-git-stu/src/main/java/io/github/zjx/javaspringbootwebstu/entity/User.java
@@ -0,0 +1,41 @@
+package io.github.zjx.javaspringbootwebstu.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String username;
+
+    private Integer age;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
